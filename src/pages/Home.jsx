513350cb1b421b332b24dd1acc946bd60fcd11dd
hoshikira18 <khuyen.dev183@gmail.com>
2025-01-07 16:54:09 +0700

    feat: add polymorphic Button component with customizable styles and props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,29 @@
 import { Link } from "react-router-dom";
+import Button from "../components/Button";
+
+const menuItems = [
+    {
+        label: "useReducer hook",
+        href: "/use-reducer",
+    },
+    {
+        label: "useReducer hook",
+        href: "/use-reducer",
+    },
+    {
+        label: "useReducer hook",
+        href: "/use-reducer",
+    },
+];
 
 const Home = () => {
     return (
-        <div>
-            <p>
-                <Link to={"/use-reducer"}>1. useReducer hook</Link>
-            </p>
+        <div className="flex flex-col gap-4">
+            {menuItems.map((item, index) => (
+                <Button key={item.href} component={Link} to={item.href}>
+                    {index + 1}. {item.label}
+                </Button>
+            ))}
         </div>
     );
 };
