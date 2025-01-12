33b7b0a765c99a18da6eed27f20cf39d649ff561
hoshikira18 <khuyen.dev183@gmail.com>
2025-01-12 11:32:18 +0700

    refactor: using router provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Redux from "./pages/Redux";
 import UseReducer from "./pages/UseReducer";
 
+const router = createBrowserRouter([
+    {
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/use-reducer",
+                element: <UseReducer />,
+            },
+            {
+                path: "/redux",
+                element: <Redux />,
+            },
+        ],
+    },
+]);
+
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="/use-reducer" element={<UseReducer />} />
-                    <Route path="/redux" element={<Redux />} />
-                </Route>
-                <Route path={"*"} element={<div>Not Found 404</div>} />
-            </Routes>
-        </BrowserRouter>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
+import { createBrowserRouter } from "react-router-dom";
 import App from "./App.js";
 import store from "./store.ts";
 
